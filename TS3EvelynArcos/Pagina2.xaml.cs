c77d09b4adbfc2c0b9d46f8d28520608c34c6adc
Gaby <Gaby@DESKTOP-QV4SGNJ>
2021-10-27 17:46:09 -0500

    Agregar archivos de proyecto.

---
+++ TS3EvelynArcos/Pagina2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TS3EvelynArcos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina2 : ContentPage
+    {
+        public Pagina2(string usuario, string clave)
+        {
+            InitializeComponent();
+            //recibe el datos
+            lblUser.Text = usuario;
+
+        }
+
+        private void btnTotal_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                double s1 = Convert.ToDouble(TxtDato1.Text);
+                double exs1 = Convert.ToDouble(TxtDato2.Text);
+                double s2 = Convert.ToDouble(TxtDato3.Text);
+                double ex2 = Convert.ToDouble(TxtDato4.Text);
+
+                double total = ((s1 * 0.3) + (exs1 * 0.2)) + ((s2 * 0.3) + (ex2 * 0.2));
+
+                TxtTotal.Text = total.ToString();
+                if (total >= 7)
+                {
+                    DisplayAlert("Feliciones", "APROBADO", "SALIR");
+                }
+                else
+                {
+                    DisplayAlert("Sigue Intentando", "REPROBADO", "SALIR");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
